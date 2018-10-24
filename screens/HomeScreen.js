821f83695605d5034ec186f3fe4eeaa2a68a2d14
Rebecca Wedow <rwedow@ufl.edu>
2018-10-24 10:56:53 -0400

    fixed a few things

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,198 +1,292 @@
-import React from 'react';
-import {
-  Image,
-  Platform,
-  ScrollView,
-  StyleSheet,
-  Text,
-  TouchableOpacity,
-  TouchableHighlight,
-  View,
-} from 'react-native';
+import React, { Component } from "react";
+import { Svg, Path } from "react-native-svg";
+// import { Center } from "@builderx/utils";
+import { View, Text, StyleSheet, TextInput } from "react-native";
 
-import { WebBrowser } from 'expo';
-import { MonoText } from '../components/StyledText';
-
-import Colors from '../constants/Colors';
-
-export default class HomeScreen extends React.Component {
-  static navigationOptions = {
-    header: null,
-  };
-
+export default class IPhone818 extends Component {
   render() {
-    const { navigate } = this.props.navigation;
     return (
-      <View style={styles.container}>
-        <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
-
-          <View style={styles.getStartedContainer}>
-            {/* {this._maybeRenderDevelopmentModeWarning()} } */}
-
-            <Text style={styles.getStartedText}>Start your first grocery list!</Text>
-
-            {/* <View style={[styles.codeHighlightContainer, styles.homeScreenFilename]}>
-              <MonoText style={styles.codeHighlightText}>screens/HomeScreen.js</MonoText>
-            </View> */}
-
-            <TouchableHighlight onPress={() => navigate('Links')}>
-              <Image
-                source={
-                  __DEV__
-                    ? require('../assets/images/shoppingIcon1.png')
-                    : require('../assets/images/shoppingIcon1.png')
-                }
-                onPress={this._startList}
-                style={styles.welcomeImage}
-              />
-            </TouchableHighlight>
-
-           
-          </View>
-
-          {/* <View style={styles.helpContainer}>
-            <TouchableOpacity onPress={this._handleHelpPress} style={styles.helpLink}>
-              <Text style={styles.helpLinkText}>Help, it didn’t automatically reload!</Text>
-            </TouchableOpacity>
-          </View> */}
-        </ScrollView>
-
-        {/* <View style={styles.tabBarInfoContainer}>
-          <Text style={styles.tabBarInfoText}>This is a tab bar. You can edit it in:</Text>
-
-          <View style={[styles.codeHighlightContainer, styles.navigationFilename]} >
-            <MonoText style={styles.codeHighlightText}>navigation/MainTabNavigator.js</MonoText>
-          </View>
-        </View> */}
+      <View style={styles.root}>
+        <View style={styles.startButton}>
+          <Svg
+            viewBox="0 0 345.00 345.00"
+            preserveAspectRatio="none"
+            style={styles.oval}
+          >
+            <Path
+              strokeWidth={0}
+              fill="rgba(255,127,102,1)"
+              isClosed={true}
+              d="M172.50 345.00 C267.75 345.00 345.00 267.75 345.00 172.50 C345.00 77.25 267.75 0.00 172.50 0.00 C77.25 0.00 0.00 77.25 0.00 172.50 C0.00 267.75 77.25 345.00 172.50 345.00 Z"
+            />
+          </Svg>
+          <Svg
+            viewBox="0 0 319.00 319.00"
+            preserveAspectRatio="none"
+            style={styles.oval2}
+          >
+            <Path
+              strokeWidth={0}
+              fill="rgba(249,87,56,1)"
+              isClosed={true}
+              d="M159.50 319.00 C247.58 319.00 319.00 247.58 319.00 159.50 C319.00 71.42 247.58 0.00 159.50 0.00 C71.42 0.00 0.00 71.42 0.00 159.50 C0.00 247.58 71.42 319.00 159.50 319.00 Z"
+            />
+          </Svg>
+          {/* <Center horizontal>
+            <Text style={styles.text}>Start Shopping!</Text>
+          </Center> */}
+        </View>
+        <BarsTabBar3Items2 style={styles.barsTabBar3Items} />
       </View>
     );
   }
-
-  _maybeRenderDevelopmentModeWarning() {
-    if (__DEV__) {
-      const learnMoreButton = (
-        <Text onPress={this._handleLearnMorePress} style={styles.helpLinkText}>
-          Learn more
-        </Text>
-      );
-
-      return (
-        <Text style={styles.developmentModeText}>
-          Development mode is enabled, your app will be slower but you can use useful development
-          tools. {learnMoreButton}
-        </Text>
-      );
-    } else {
-      return (
-        <Text style={styles.developmentModeText}>
-          You are not in development mode, your app will run at full speed.
-        </Text>
-      );
-    }
-  }
-
-  _startList = () => {
-    navigate('Links', {name: Jane});
-  }
-
-  _handleLearnMorePress = () => {
-    WebBrowser.openBrowserAsync('https://docs.expo.io/versions/latest/guides/development-mode');
-  };
-
-  _handleHelpPress = () => {
-    WebBrowser.openBrowserAsync(
-      'https://docs.expo.io/versions/latest/guides/up-and-running.html#can-t-see-your-changes'
-    );
-  };
 }
-
 const styles = StyleSheet.create({
-
-  container: {
-    flex: 1,
-    backgroundColor: '#FAF0CA',
+  root: {
+    backgroundColor: "rgba(250,240,202,1)",
+    flex: 1
   },
-  developmentModeText: {
-    marginBottom: 20,
-    color: 'rgba(0,0,0,0.4)',
-    fontSize: 14,
-    lineHeight: 19,
-    textAlign: 'center',
+  startButton: {
+    position: "absolute",
+    top: "26.63%",
+    left: "8.45%",
+    height: "46.88%",
+    width: "83.33%"
   },
-  contentContainer: {
-    paddingTop: 30,
+  oval: {
+    position: "absolute",
+    height: "100.00%",
+    width: "100.00%",
+    top: "0.00%",
+    left: "0.00%",
+    backgroundColor: "transparent",
+    borderColor: "transparent"
   },
-  welcomeContainer: {
-    alignItems: 'center',
-    marginTop: 10,
-    marginBottom: 20,
+  oval2: {
+    position: "absolute",
+    height: "92.46%",
+    width: "92.46%",
+    top: "3.77%",
+    left: "3.77%",
+    backgroundColor: "transparent",
+    borderColor: "transparent"
   },
-  welcomeImage: {
-    // width: 100,
-    height: 200,
-    resizeMode: 'contain',
-    marginTop: 10,
-    marginLeft: -10,
+  text: {
+    position: "absolute",
+    height: 159,
+    width: 266,
+    backgroundColor: "transparent",
+    textAlign: "center",
+    color: "rgba(250,240,202,1)",
+    fontSize: 55,
+    fontFamily: "Futura-Medium",
+    top: "27.21%"
   },
-  getStartedContainer: {
-    alignItems: 'center',
-    marginHorizontal: 50,
-  },
-  homeScreenFilename: {
-    marginVertical: 7,
-  },
-  codeHighlightText: {
-    color: 'rgba(96,100,109, 0.8)',
-  },
-  codeHighlightContainer: {
-    backgroundColor: 'rgba(0,0,0,0.05)',
-    borderRadius: 3,
-    paddingHorizontal: 4,
-  },
-  getStartedText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    lineHeight: 24,
-    textAlign: 'center',
-    marginTop: 100
-  },
-  tabBarInfoContainer: {
-    position: 'absolute',
-    bottom: 0,
-    left: 0,
-    right: 0,
-    ...Platform.select({
-      ios: {
-        shadowColor: 'black',
-        shadowOffset: { height: -3 },
-        shadowOpacity: 0.1,
-        shadowRadius: 3,
-      },
-      android: {
-        elevation: 20,
-      },
-    }),
-    alignItems: 'center',
-    // backgroundColor: '#fbfbfb',
-    paddingVertical: 20,
-  },
-  tabBarInfoText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    textAlign: 'center',
-  },
-  navigationFilename: {
-    marginTop: 5,
-  },
-  helpContainer: {
-    marginTop: 15,
-    alignItems: 'center',
-  },
-  helpLink: {
-    paddingVertical: 15,
-  },
-  helpLinkText: {
-    fontSize: 14,
-    color: '#2e78b7',
-  },
+  barsTabBar3Items: {
+    position: "absolute",
+    top: "92.26%",
+    left: "0.00%",
+    height: "7.74%",
+    width: "100.00%",
+    backgroundColor: "transparent"
+  }
 });
+
+// import React from 'react';
+// import {
+//   Image,
+//   Platform,
+//   ScrollView,
+//   StyleSheet,
+//   Text,
+//   TouchableOpacity,
+//   TouchableHighlight,
+//   View,
+// } from 'react-native';
+
+// import { WebBrowser } from 'expo';
+// import { MonoText } from '../components/StyledText';
+
+// import Colors from '../constants/Colors';
+
+// export default class HomeScreen extends React.Component {
+//   static navigationOptions = {
+//     header: null,
+//   };
+
+//   render() {
+//     const { navigate } = this.props.navigation;
+//     return (
+//       <View style={styles.container}>
+//         <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
+
+//           <View style={styles.getStartedContainer}>
+//             {/* {this._maybeRenderDevelopmentModeWarning()} } */}
+
+//             <Text style={styles.getStartedText}>Start your first grocery list!</Text>
+
+//             {/* <View style={[styles.codeHighlightContainer, styles.homeScreenFilename]}>
+//               <MonoText style={styles.codeHighlightText}>screens/HomeScreen.js</MonoText>
+//             </View> */}
+
+//             <TouchableHighlight onPress={() => navigate('Links')}>
+//               <Image
+//                 source={
+//                   __DEV__
+//                     ? require('../assets/images/shoppingIcon1.png')
+//                     : require('../assets/images/shoppingIcon1.png')
+//                 }
+//                 onPress={this._startList}
+//                 style={styles.welcomeImage}
+//               />
+//             </TouchableHighlight>
+
+           
+//           </View>
+
+//           {/* <View style={styles.helpContainer}>
+//             <TouchableOpacity onPress={this._handleHelpPress} style={styles.helpLink}>
+//               <Text style={styles.helpLinkText}>Help, it didn’t automatically reload!</Text>
+//             </TouchableOpacity>
+//           </View> */}
+//         </ScrollView>
+
+//         {/* <View style={styles.tabBarInfoContainer}>
+//           <Text style={styles.tabBarInfoText}>This is a tab bar. You can edit it in:</Text>
+
+//           <View style={[styles.codeHighlightContainer, styles.navigationFilename]} >
+//             <MonoText style={styles.codeHighlightText}>navigation/MainTabNavigator.js</MonoText>
+//           </View>
+//         </View> */}
+//       </View>
+//     );
+//   }
+
+//   _maybeRenderDevelopmentModeWarning() {
+//     if (__DEV__) {
+//       const learnMoreButton = (
+//         <Text onPress={this._handleLearnMorePress} style={styles.helpLinkText}>
+//           Learn more
+//         </Text>
+//       );
+
+//       return (
+//         <Text style={styles.developmentModeText}>
+//           Development mode is enabled, your app will be slower but you can use useful development
+//           tools. {learnMoreButton}
+//         </Text>
+//       );
+//     } else {
+//       return (
+//         <Text style={styles.developmentModeText}>
+//           You are not in development mode, your app will run at full speed.
+//         </Text>
+//       );
+//     }
+//   }
+
+//   _startList = () => {
+//     navigate('Links', {name: Jane});
+//   }
+
+//   _handleLearnMorePress = () => {
+//     WebBrowser.openBrowserAsync('https://docs.expo.io/versions/latest/guides/development-mode');
+//   };
+
+//   _handleHelpPress = () => {
+//     WebBrowser.openBrowserAsync(
+//       'https://docs.expo.io/versions/latest/guides/up-and-running.html#can-t-see-your-changes'
+//     );
+//   };
+// }
+
+// const styles = StyleSheet.create({
+
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#FAF0CA',
+//   },
+//   developmentModeText: {
+//     marginBottom: 20,
+//     color: 'rgba(0,0,0,0.4)',
+//     fontSize: 14,
+//     lineHeight: 19,
+//     textAlign: 'center',
+//   },
+//   contentContainer: {
+//     paddingTop: 30,
+//   },
+//   welcomeContainer: {
+//     alignItems: 'center',
+//     marginTop: 10,
+//     marginBottom: 20,
+//   },
+//   welcomeImage: {
+//     // width: 100,
+//     height: 200,
+//     resizeMode: 'contain',
+//     marginTop: 10,
+//     marginLeft: -10,
+//   },
+//   getStartedContainer: {
+//     alignItems: 'center',
+//     marginHorizontal: 50,
+//   },
+//   homeScreenFilename: {
+//     marginVertical: 7,
+//   },
+//   codeHighlightText: {
+//     color: 'rgba(96,100,109, 0.8)',
+//   },
+//   codeHighlightContainer: {
+//     backgroundColor: 'rgba(0,0,0,0.05)',
+//     borderRadius: 3,
+//     paddingHorizontal: 4,
+//   },
+//   getStartedText: {
+//     fontSize: 17,
+//     color: 'rgba(96,100,109, 1)',
+//     lineHeight: 24,
+//     textAlign: 'center',
+//     marginTop: 100
+//   },
+//   tabBarInfoContainer: {
+//     position: 'absolute',
+//     bottom: 0,
+//     left: 0,
+//     right: 0,
+//     ...Platform.select({
+//       ios: {
+//         shadowColor: 'black',
+//         shadowOffset: { height: -3 },
+//         shadowOpacity: 0.1,
+//         shadowRadius: 3,
+//       },
+//       android: {
+//         elevation: 20,
+//       },
+//     }),
+//     alignItems: 'center',
+//     // backgroundColor: '#fbfbfb',
+//     paddingVertical: 20,
+//   },
+//   tabBarInfoText: {
+//     fontSize: 17,
+//     color: 'rgba(96,100,109, 1)',
+//     textAlign: 'center',
+//   },
+//   navigationFilename: {
+//     marginTop: 5,
+//   },
+//   helpContainer: {
+//     marginTop: 15,
+//     alignItems: 'center',
+//   },
+//   helpLink: {
+//     paddingVertical: 15,
+//   },
+//   helpLinkText: {
+//     fontSize: 14,
+//     color: '#2e78b7',
+//   },
+// });
--- screens/ListsScreen.js
+++ screens/ListsScreen.js
@@ -1,15 +1,81 @@
 import React, { Component } from "react";
-import { View, Text, StyleSheet, TextInput } from "react-native";
+import BarsTabBar3Items2 from "../symbols/BarsTabBar3Items2";
+import { Svg, Path } from "react-native-svg";
+import { View, Text, StyleSheet } from "react-native";
 
-export default class IPhone8 extends Component {
+export default class IPhone820 extends Component {
   render() {
     return (
       <View style={styles.root}>
-        <Text style={styles.addItems}>Add items:</Text>
-        <TextInput id="searchInput" placeholder="TextInput" style={styles.textInput} />
+        <BarsTabBar3Items2 style={styles.barsTabBar3Items} />
+        <Svg
+          viewBox="0 0 423.00 4.00"
+          preserveAspectRatio="none"
+          style={styles.line}
+        >
+          <Path
+            strokeWidth={1}
+            fill="rgba(0,0,0,1)"
+            stroke="rgba(255,255,255,1)"
+            isClosed={false}
+            d="M1.01 1.50 L420.99 1.50 "
+          />
+        </Svg>
         <View style={styles.group}>
-        {/* iterate the results here */}
-          <Text style={styles.searchResults}>Annie’s Tomato Soup</Text>
+          <View style={styles.rectangle} />
+          <Text style={styles.recipes}>Recipes</Text>
+        </View>
+        <View style={styles.group1}>
+          <View style={styles.rectangle1} />
+          <Text style={styles.coupons}>Coupons</Text>
+        </View>
+        <View style={styles.group2}>
+          <Svg
+            viewBox="0 0 423.00 4.00"
+            preserveAspectRatio="none"
+            style={styles.line2}
+          >
+            <Path
+              strokeWidth={1}
+              fill="rgba(0,0,0,1)"
+              stroke="rgba(255,255,255,1)"
+              isClosed={false}
+              d="M1.01 1.50 L420.99 1.50 "
+            />
+          </Svg>
+          <Text style={styles.tomatoSoup}>Tomato Soup</Text>
+        </View>
+        <View style={styles.group3}>
+          <Svg
+            viewBox="0 0 423.00 4.00"
+            preserveAspectRatio="none"
+            style={styles.line4}
+          >
+            <Path
+              strokeWidth={1}
+              fill="rgba(0,0,0,1)"
+              stroke="rgba(255,255,255,1)"
+              isClosed={false}
+              d="M1.01 1.50 L420.99 1.50 "
+            />
+          </Svg>
+          <Text style={styles.tacoSoup}>Taco Soup</Text>
+          <View style={styles.group4}>
+            <Svg
+              viewBox="0 0 423.00 4.00"
+              preserveAspectRatio="none"
+              style={styles.line6}
+            >
+              <Path
+                strokeWidth={1}
+                fill="rgba(0,0,0,1)"
+                stroke="rgba(255,255,255,1)"
+                isClosed={false}
+                d="M1.01 1.50 L420.99 1.50 "
+              />
+            </Svg>
+            <Text style={styles.fermentedSalsa}>Fermented Salsa</Text>
+          </View>
         </View>
       </View>
     );
@@ -20,25 +86,100 @@ const styles = StyleSheet.create({
     backgroundColor: "rgba(250,240,202,1)",
     flex: 1
   },
-  addItems: {
+  barsTabBar3Items: {
     position: "absolute",
-    top: "3.13%",
-    left: "2.64%",
-    height: "10.94%",
-    width: "83.06%",
-    color: "rgba(249,87,56,1)",
-    fontSize: 42,
-    fontFamily: "Futura-Medium",
-    letterSpacing: 2.44
+    top: "92.26%",
+    left: "0.00%",
+    height: "7.74%",
+    width: "100.00%",
+    backgroundColor: "transparent"
+  },
+  line: {
+    position: "absolute",
+    height: "0.41%",
+    width: "101.93%",
+    top: "19.23%",
+    left: "-0.36%",
+    backgroundColor: "transparent",
+    borderColor: "transparent"
   },
   group: {
     position: "absolute",
-    top: "22.19%",
-    height: "6.88%",
-    width: 366,
-    left: "2.78%"
+    top: "2.45%",
+    left: "9.66%",
+    height: "5.57%",
+    width: "33.57%"
   },
-  searchResults: {
+  rectangle: {
+    position: "absolute",
+    top: "0.00%",
+    left: "0.00%",
+    height: "100.00%",
+    width: "100.00%",
+    borderRadius: 20.5,
+    backgroundColor: "rgba(238,150,75,1)"
+  },
+  recipes: {
+    position: "absolute",
+    top: "0.00%",
+    left: "10.07%",
+    height: "87.80%",
+    width: "79.86%",
+    color: "rgba(255,255,255,1)",
+    textAlign: "center",
+    fontSize: 28,
+    fontFamily: "Futura-Medium",
+    letterSpacing: 1.42
+  },
+  group1: {
+    position: "absolute",
+    top: "2.45%",
+    left: "50.48%",
+    height: "5.57%",
+    width: "40.10%",
+    opacity: 0.63
+  },
+  rectangle1: {
+    position: "absolute",
+    top: "0.00%",
+    left: "0.00%",
+    height: "100.00%",
+    width: "100.00%",
+    borderRadius: 20.5,
+    backgroundColor: "rgba(204,204,204,1)"
+  },
+  coupons: {
+    position: "absolute",
+    top: "0.00%",
+    left: "10.24%",
+    height: "87.80%",
+    width: "80.12%",
+    color: "rgba(167,167,167,1)",
+    textAlign: "center",
+    fontSize: 28,
+    fontFamily: "Futura-Medium",
+    letterSpacing: 1.42
+  },
+  group2: {
+    position: "absolute",
+    top: "20.52%",
+    left: "0.00%",
+    height: "6.93%",
+    width: "101.69%"
+  },
+  line2: {
+    position: "absolute",
+    height: "5.88%",
+    width: "100.24%",
+    top: "95.10%",
+    left: "-0.12%",
+    backgroundColor: "transparent",
+    borderColor: "transparent"
+  },
+  tomatoSoup: {
+    position: "absolute",
+    top: "0.00%",
+    left: "4.28%",
     height: "78.43%",
     width: "90.26%",
     backgroundColor: "transparent",
@@ -47,15 +188,62 @@ const styles = StyleSheet.create({
     fontFamily: "Futura-Medium",
     letterSpacing: 1.42
   },
-  textInput: {
-    height: 41,
-    width: 317,
-    top: 90,
+  group3: {
     position: "absolute",
-    borderWidth: 5,
-    borderColor: "rgba(255,252,252,1)",
-    left: "2.78%"
+    top: "28.53%",
+    left: "0.00%",
+    height: "15.08%",
+    width: "101.69%"
   },
+  line4: {
+    position: "absolute",
+    height: "2.70%",
+    width: "100.24%",
+    top: "43.69%",
+    left: "-0.12%",
+    backgroundColor: "transparent",
+    borderColor: "transparent"
+  },
+  tacoSoup: {
+    position: "absolute",
+    top: "0.00%",
+    left: "4.28%",
+    height: "36.04%",
+    width: "90.26%",
+    backgroundColor: "transparent",
+    color: "rgba(106,106,106,1)",
+    fontSize: 28,
+    fontFamily: "Futura-Medium",
+    letterSpacing: 1.42
+  },
+  group4: {
+    position: "absolute",
+    top: "54.05%",
+    left: "0.00%",
+    height: "45.95%",
+    width: "100.00%"
+  },
+  line6: {
+    position: "absolute",
+    height: "5.88%",
+    width: "100.24%",
+    top: "95.10%",
+    left: "-0.12%",
+    backgroundColor: "transparent",
+    borderColor: "transparent"
+  },
+  fermentedSalsa: {
+    position: "absolute",
+    top: "0.00%",
+    left: "4.28%",
+    height: "78.43%",
+    width: "90.26%",
+    backgroundColor: "transparent",
+    color: "rgba(106,106,106,1)",
+    fontSize: 28,
+    fontFamily: "Futura-Medium",
+    letterSpacing: 1.42
+  }
 });
 
 
